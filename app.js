d477512e894ee5f082c5b269a45f3eede2490497
orlandopdj <orlandodiasdev@gmail.com>
2023-07-28 23:31:24 -0300

    diminuindo algumas linhas de código

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 
 
-const quadrado = document.querySelectorAll('.quadrado')
+const quadrados = document.querySelectorAll('.quadrado') // ELA OBTEM TODOS OS QUADRADOS COM [index]
 const container = document.querySelector('.container')
 const btn = document.querySelector('.btn')
 const botao = document.createElement("button");
-let contador = 0 // ESSA LET SERVE PRA FAZER A VERIFICAR O JOGADOR 
+let contador = 0 // ESSA LET SERVE PARA VERIFICAR O JOGADOR 
 
 // ---------------------------------------------------------------
 
-let item1 = document.querySelector('.item-1')
-let item2 = document.querySelector('.item-2')
-let item3 = document.querySelector('.item-3')
-let item4 = document.querySelector('.item-4')
-let item5 = document.querySelector('.item-5')
-let item6 = document.querySelector('.item-6')
-let item7 = document.querySelector('.item-7')
-let item8 = document.querySelector('.item-8')
-let item9 = document.querySelector('.item-9')
-
-
+// let item1 = document.querySelector('.item-1')
+// let item2 = document.querySelector('.item-2')
+// let item3 = document.querySelector('.item-3')
+// let item4 = document.querySelector('.item-4')
+// let item5 = document.querySelector('.item-5')
+// let item6 = document.querySelector('.item-6')  
+// let item7 = document.querySelector('.item-7')
+// let item8 = document.querySelector('.item-8')
+// let item9 = document.querySelector('.item-9')
 
 // ESSA LET ITEM ELA BUSCA TODOS OS 9 QUADRADOS DO JOGO DA VELHA
+
+// ---------------------OLD CODE---------------------------------
+
+
 
 
 
@@ -44,6 +46,8 @@ function click(e) {
         return
     }
     alert('Você não pode adicionar aqui')
+    
+    console.log[quadrados]
 }
 
 
@@ -53,7 +57,7 @@ const reiniciar = () => {
 }
 
 const winner = () => {
-    if (item1.innerText === 'X' && item4.innerText === 'X' && item7.innerText === 'X' || item1.innerText === 'O' && item4.innerText === 'O' && item7.innerText === 'O') {
+    if (quadrados[0].innerText === 'X' && quadrados[3].innerText === 'X' && quadrados[6].innerText === 'X' || quadrados[0].innerText === 'O' && quadrados[3].innerText === 'O' && quadrados[6].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
@@ -62,44 +66,44 @@ const winner = () => {
         // container.appendChild(botao)
 
     }
-    else if (item2.innerText === 'X' && item5.innerText === 'X' && item8.innerText === 'X' || item2.innerText === 'O' && item5.innerText === 'O' && item8.innerText === 'O') {
+    else if (quadrados[1].innerText === 'X' && quadrados[4].innerText === 'X' && quadrados[7].innerText === 'X' || quadrados[1].innerText === 'O' && quadrados[4].innerText === 'O' && quadrados[7].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
-    else if (item3.innerText === 'X' && item6.innerText === 'X' && item9.innerText === 'X' || item3.innerText === 'O' && item6.innerText === 'O' && item9.innerText === 'O') {
+    else if (quadrados[2].innerText === 'X' && quadrados[5].innerText === 'X' && quadrados[8].innerText === 'X' || quadrados[2].innerText === 'O' && quadrados[5].innerText === 'O' && quadrados[8].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
-    else if (item1.innerText === 'X' && item2.innerText === 'X' && item3.innerText === 'X' || item1.innerText === 'O' && item2.innerText === 'O' && item3.innerText === 'O') {
+    else if (quadrados[0].innerText === 'X' && quadrados[1].innerText === 'X' && quadrados[2].innerText === 'X' || quadrados[0].innerText === 'O' && quadrados[1].innerText === 'O' && quadrados[2].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
-    else if (item4.innerText === 'X' && item5.innerText === 'X' && item6.innerText === 'X' || item5.innerText === 'O' && item4.innerText === 'O' && item6.innerText === 'O') {
+    else if (quadrados[3].innerText === 'X' && quadrados[4].innerText === 'X' && quadrados[5].innerText === 'X' || quadrados[4].innerText === 'O' && quadrados[3].innerText === 'O' && quadrados[5].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
 
-    else if (item7.innerText === 'X' && item8.innerText === 'X' && item9.innerText === 'X' || item7.innerText === 'O' && item8.innerText === 'O' && item9.innerText === 'O') {
+    else if (quadrados[6].innerText === 'X' && quadrados[7].innerText === 'X' && quadrados[8].innerText === 'X' || quadrados[6].innerText === 'O' && quadrados[7].innerText === 'O' && quadrados[8].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
-    else if (item1.innerText === 'X' && item5.innerText === 'X' && item9.innerText === 'X' || item1.innerText === 'O' && item5.innerText === 'O' && item9.innerText === 'O') {
+    else if (quadrados[0].innerText === 'X' && quadrados[4].innerText === 'X' && quadrados[8].innerText === 'X' || quadrados[0].innerText === 'O' && quadrados[4].innerText === 'O' && quadrados[8].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
     </div>`
     }
-    else if (item7.innerText === 'X' && item5.innerText === 'X' && item3.innerText === 'X' || item7.innerText === 'O' && item5.innerText === 'O' && item3.innerText === 'O') {
+    else if (quadrados[6].innerText === 'X' && quadrados[4].innerText === 'X' && quadrados[2].innerText === 'X' || quadrados[6].innerText === 'O' && quadrados[4].innerText === 'O' && quadrados[2].innerText === 'O') {
         container.innerHTML = `<h2> PARABÉNS VOCÊ GANHOU! </h2>`
         btn.innerHTML = `<div class="btn">
         <button class="restart" onclick="reiniciar()">Jogar novamente!</button>
@@ -112,8 +116,7 @@ const winner = () => {
 
 
 
-quadrado.forEach((quadrado) => { // VERIFICA O EVENTO DE TODOS OS QUADRADOS
+quadrados.forEach((quadrado) => { // VERIFICA O EVENTO DE TODOS OS QUADRADOS
     quadrado.addEventListener('click', click);
 
 });
-
